3ddcf1e43644aff9bba189c4dd2a34fa76fbcd13
leesuehee <leesuehee@outlook.com>
2018-03-28 19:46:46 -0700

    Configures the following files to handle multiple services

--- templates/app.js
+++ templates/app.js
@@ -1,4 +1,9 @@
 
-module.exports = (Overview) => `
+module.exports = (Overview, Photos, Sidebar, Recommendations) => `
   <div id="Overview">${Overview}</div>
+  <div id="Photos">${Photos}</div>
+  <div id="Sidebar">${Sidebar}</div>
+  <div id="Recommendations">${Recommendations}</div>
 `;
+
+
